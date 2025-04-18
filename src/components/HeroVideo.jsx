1fdfc43fa93aa27f9c4c16c2bbcbe949876c2c37
Yosuf AlAwadi <yosuf.alawadi@student.manchester.ac.uk>
2025-04-18 18:22:19 +0200

    changed backround and hero

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -1,5 +1,3 @@
-import logo from "../assets/logo.png"; // Update path if necessary
-
 export default function HeroVideo() {
   return (
     <div
@@ -8,28 +6,21 @@ export default function HeroVideo() {
         height: "calc(100vh - 7rem)",
       }}
     >
-      {/* Background layer */}
+      {/* Background layer for mobile */}
       <div
-        className="absolute inset-0 bg-no-repeat bg-cover bg-center md:bg-[center_60%]"
+        className="absolute inset-0 bg-no-repeat bg-cover bg-center"
         style={{
           backgroundImage: `url('/hero1.png')`,
         }}
       >
         {/* Override on desktop */}
         <div
-          className="hidden md:block absolute inset-0 bg-no-repeat bg-cover bg-[center_60%]"
+          className="hidden md:block absolute inset-0 bg-no-repeat bg-contain bg-center"
           style={{
             backgroundImage: `url('/desktop.png')`,
           }}
         ></div>
       </div>
-
-      {/* Logo (desktop only) */}
-      <img
-        src={logo}
-        alt="Cinema Al Balad"
-        className="hidden md:block relative z-10 w-72 sm:w-96 md:w-[500px] animate-fadeIn drop-shadow-[0_0_15px_rgba(0,0,0,0.6)]"
-      />
     </div>
   );
 }
